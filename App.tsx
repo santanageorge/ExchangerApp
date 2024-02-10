132f22a39991653b763ee5c6514e2ba693998297
Santana George <santanageorge@outlook.com>
2024-02-10 13:01:19 +0530

    sa

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  
+  const getData = async()=>{
+    const apiUrl = 'http://10.0.2.2:5104'
+    const response = await axios.get(apiUrl+"/WeatherForecast");
+    console.log(response.data);
+  }
+  getData();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
